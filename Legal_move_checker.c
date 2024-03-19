16ee763a6c72712ab458c764b782cf33fee1d308
Sadra-Sh <sadra.shirdarrreh@mail.utoronto.ca>
2024-03-19 15:55:38 -0400

    update

--- Legal_move_checker.c
+++ Legal_move_checker.c
@@ -11,7 +11,8 @@ void print_board();
 void check_turn(); // this checks if the turn inputed by the user is valid, swaps turns if it is
 void position_legal(); // this checks if the piece is at the start end locations are valid, if the destination is able to be 
                     // reached by the piece in one move, and if there are peices around it.
-void is_L_shaped(); // checks if the move entered for a knight is L shaped
+void obstructed_path(); // checks if the piece being moved has other pieces in its path, if so move is not legal unless its a knight
+void update_board();
 
 /***************************************************************************************************************************
  ******************************************************** GLOBALS ***********************************************************
@@ -26,7 +27,7 @@ char Board[8][8] = {
 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 
 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}; // capital letters resemble black pieces and lower case letter resemble white pieces
 
-char move [6] = {'P', 'A', '3'}; // 3 characters (piece, x1, y1, x2, y2) + null terminator
+char move [6] = {'P', 'a', '3', 'a', '4'}; // 3 characters (piece, x1, y1, x2, y2) + null terminator
 char turn = 'w';
 bool legal_move = false;
 
@@ -70,55 +71,123 @@ void position_legal(){
     if (legal_move == true ){
 
         if (move[0] == 'R' || move[0]=='r'){
-            if (move[1] != move[3] || move[2] != move[4]) // if the move is not horizontal or diagronal
-                legal_move = false;
-        }
-        if (move[0] == 'K' || move[0] == 'k'){
             int deltax = abs(move[1] - move[3]); // |x1 - x2|
             int deltay = abs(move[2] - move[4]); // |y1 - y2|
-
-            if (!(deltax == 2 && deltay == 1) || !(deltax == 1 && deltay == 2))
+            if (deltax != 0|| deltay != 0){ // if the move is not horizontal or vertical
                 legal_move = false;
+                return;
+            }
+        }
+        if (move[0] == 'N' || move[0] == 'n'){
+            int deltax = abs(move[1] - move[3]); // |x1 - x2|
+            int deltay = abs(move[2] - move[4]); // |y1 - y2|
+            if (!(deltax == 2 && deltay == 1) || !(deltax == 1 && deltay == 2)){ // if the move is not L shaped
+                legal_move = false;
+                return;
+            }
         }
         if (move[0] == 'B' || move[0] == 'b'){
             int deltax = abs(move[1] - move[3]); // |x1 - x2|
             int deltay = abs(move[2] - move[4]); // |y1 - y2|
-
-            if (deltax != deltay)
+            if (deltax != deltay){ // if the move is not diagonal
                 legal_move = false;
+                return;
+            }
         }
         if (move[0] == 'K' || move[0] == 'k'){
             int deltax = abs(move[1] - move[3]); // |x1 - x2|
             int deltay = abs(move[2] - move[4]); // |y1 - y2|
-            if (deltax != 1 || deltay != 1)
+            if (deltax != 1 || deltay != 1){ // move is not by 1 square
                 legal_move = false;
+                return;
+            }
         }
         if (move[0] == 'Q' || move[0] == 'q'){
             int deltax = abs(move[1] - move[3]); // |x1 - x2|
             int deltay = abs(move[2] - move[4]); // |y1 - y2|
-            if (deltax != deltay || deltax == 0 || deltay == 0)
+            if (deltax != deltay || deltax == 0 || deltay == 0){ // if move is not horitzontal, veritical, or diagonal
                 legal_move = false;
+                return;
+            }
         }
         if (move[0] == 'P' || move[0] == 'p'){
             int deltax = move[3] - move[1]; // |x1 - x2| - cannot go backwards 
             int deltay = move[4] - move[2]; // |y1 - y2|
 
-            if (move[0] == 'p' && move[1] == 2){ // if it is a white pawn and it has not been moved
-                if (!(deltay == 1 || deltay == 2) && deltax != 0)
-                    legal_move = false;
+            if (move[0] == 'p'){ // WHITE pawn
+                if (move[1] == 2 ){ // if it is a white pawn and it has not been moved
+                    if (!(deltay == 1 || deltay == 2) && deltax != 0){ 
+                        legal_move = false;
+                        return;
+                    }
+                    else 
+                        obstructed_path();
+                }
+                else {
+                    if (!(deltay == 1) && deltax != 0){ // if not the the first move, then one 1 square moves allowed
+                        legal_move = false;
+                        return;
+                    }
+                    else 
+                        obstructed_path();
+                }
             }
-            if (move[0] == 'P' && move[1] == 7){ // if it is a black pawn and it has not been moved
-                if (!(deltay == -1 || deltay == -2) && deltax != 0)
-                    legal_move = false;
-            }
-        }
-
-        for (int i=0; i<8; i++){
-            for (int j=0; j<8; j++){
-            
+            if (move[0] == 'P'){ // BLACK pawn
+                if (move[1] == 7){ // if it is a black pawn and it has not been moved
+                    if (!(deltay == -1 || deltay == -2) && deltax != 0){
+                        legal_move = false;
+                        return;
+                    }
+                    else 
+                        obstructed_path();
+                }
+                else {
+                    if (!(deltay == 1) && deltax != 0){ // if not the the first move, then one 1 square moves allowed
+                        legal_move = false;
+                        return;
+                    }
+                    else 
+                        obstructed_path();
+                }
             }
         }
     }
+}
+
+void obstructed_path(){
+    int posx = move[1] - 1; // positions that we are checking
+    int posy = move[3] - 1;
+
+    int deltax = move[3] - move[1];
+    int deltay = move[4] - move[2];
+
+    int distancex = abs(deltax); // the distance of the path in the x direction
+    int distancey = abs(deltay); // the distance in the y direction
+
+    if (deltax > 0) // setting up the direction of movement
+        deltax = 1;
+    if (deltax < 0)
+        deltax = -1;
+    if (deltay > 0)
+        deltay = 1;
+    if (deltay < 0)
+        deltay = -1;
+
+   while (distancex != 0 && distancey != 0){
+        if (Board[posx + deltax][posy + deltay] != 'o'){ // a piece is in the way, move not legal
+            if (posx == move[3] && posy == move[4]) // if there is a piece at the destination, it is eliminated 
+                update_board();
+            posx += deltax;
+            posy + deltay;
+            legal_move = false;
+            return;
+        }
+   }
+}
+
+void update_board(){
+    Board[move[3] - 1][move[4] - 1] = move[0]; // move piece to the destination
+    Board[move[1] - 1][move[2] - 1] = 'o'; // set the old square to empty
 }
 
 void print_board(){
