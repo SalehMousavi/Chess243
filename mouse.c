22602b7df242946f0437d5c438f869d9cb7a292c
Sadra Shirdarreh <143111135+Sadra-Sh@users.noreply.github.com>
2024-04-07 16:23:10 -0400

    Update mouse.c

--- mouse.c
+++ mouse.c
@@ -24,38 +24,43 @@ void mouse_ISR() {
 		byte1 = (char)byte2;
 		byte2 = (char)byte3;
 		byte3 = (char)(PS2_data & 0x0FF);
-		if ((byte2 == (char)0xAA) && (byte3 == (char)0x00)) // mouse inserted; initialize sending of data 
-			*(PS2_ptr) = 0xF4;
-		else if(byte1 == 0x8 || byte1 == 0x18 || byte1 == 0x28 || byte1 == 0x38) {
-			if((mousex != 319 || byte2 < 0 )&&(mousex != 0 || byte2 > 0)) {
-				mousex += byte2;
+		if(mouseCount == 3) {
+			if ((byte2 == (char)0xAA) && (byte3 == (char)0x00)) // mouse inserted; initialize sending of data 
+				*(PS2_ptr) = 0xF4;
+			else if(byte1 == 0x8 || byte1 == 0x18 || byte1 == 0x28 || byte1 == 0x38) {
+				if((mousex != 319 || byte2 < 0 )&&(mousex != 0 || byte2 > 0)) {
+					mousex += byte2;
+				}
+				if((mousey != 239 || byte3 > 0 )&&(mousey != 0 || byte3 < 0)) {
+					mousey -= byte3;
+				}
+				mouseBuffer = 0;
 			}
-			if((mousey != 239 || byte3 > 0 )&&(mousey != 0 || byte3 < 0)) {
-				mousey -= byte3;
+			else if(byte1 == 0x9 || byte1 == 0x19 || byte1 == 0x29 || byte1 == 0x39) {
+				if((mousex != 319 || byte2 < 0 )&&(mousex != 0 || byte2 > 0)) {
+					mousex += byte2;
+				}
+				if((mousey != 239 || byte3 > 0 )&&(mousey != 0 || byte3 < 0)) {
+					mousey -= byte3;
+				}
+				mousePressed = 1;
+				*(PS2_ptr+1) = 0;
+				mouseBuffer = 0;
+				//move has been selected
 			}
-			mouseBuffer = 0;
+			else if(byte1 == 0xA || byte1 == 0x1A || byte1 == 0x2A || byte1 == 0x3A) {
+				mousePressed = 1;
+				mouseBuffer = 0;
+				*(PS2_ptr+1) = 0;
+				undoMove = 1;
+	
+			}	//for checking if right clicker is pressed in that case reset their move
+			mouseCount = 1;
+			
 		}
-		else if(byte1 == 0x9 || byte1 == 0x19 || byte1 == 0x29 || byte1 == 0x39) {
-			if((mousex != 319 || byte2 < 0 )&&(mousex != 0 || byte2 > 0)) {
-				mousex += byte2;
-			}
-			if((mousey != 239 || byte3 > 0 )&&(mousey != 0 || byte3 < 0)) {
-				mousey -= byte3;
-			}
-			mousePressed = 1;
-			*(PS2_ptr+1) = 0;
-			mouseBuffer = 0;
-			//move has been selected
-		}
-		else if(byte1 == 0xA || byte1 == 0x1A || byte1 == 0x2A || byte1 == 0x3A) {
-			mousePressed = 1;
-			mouseBuffer = 0;
-			*(PS2_ptr+1) = 0;
-			undoMove = 1;
-
-		}	//for checking if right clicker is pressed in that case reset their move
 		else {
 			mouseBuffer = ((byte1&0xFF) << 16) + ((byte2&0xFF) << 8) + (byte3&0xFF);
+			mouseCount++;
 		}
 	}
 	return;
